6bb6c925008da1dbbcd22bda324d3acdc4daaa17
guynawapas <56993100+guynawapas@users.noreply.github.com>
2022-02-07 14:29:36 +0700

    test_posSize

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from binance.client import Client
 from binance.enums import *
 import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -29,14 +30,40 @@ def hello_world():
 @app.route("/test1")
 def test1():
     return "<p>TEST1 Duhh</p>"
-@app.route("/test2")
-def test2():
-    return "<p>"+config.API_KEY+"</p>"
+
+@app.route("/test/getAccount")
+def getAccount():
+    client = Client(config.API_KEY,config.API_SECRET)
+    info = client.get_account()
+    data = pd.DataFrame(info["balances"])
+    usdt = data[data["asset"]=="USDT"]
+    btc = data[data["asset"]=="BTC"]
+    print(usdt)
+    print(btc)
+    return info
+
+@app.route("/test/posSize", methods=['POST'])
+def posSize():
+    client = Client(config.API_KEY,config.API_SECRET)
+    info = client.get_account()
+    df = pd.DataFrame(info["balances"])
+    usdt = df[df["asset"]=="USDT"].to_dict('records')[0]
+
+    data = json.loads(request.data)
+    risk = data['strategy']['risk']
+    slper = data['strategy']['slper']
+    equity = float(usdt['free'])
+    if equity < 1 :
+        equity = 18998.3
+    positionSize = equity*risk/slper
+    print(positionSize)
+
+    return str(positionSize)
 
 @app.route("/test/webhook", methods=['POST'])
 def testWebhook():
     data = json.loads(request.data)
-    return "I got this data\n" + data
+    return "I got this data\n" + json.dumps(data)
 
 
 
