9fc8c0cc8121e4d2f3dc6c039921e9906f8f7d01
guynawapas <56993100+guynawapas@users.noreply.github.com>
2022-02-21 14:45:54 +0700

    add notify

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ WEBHOOK_PASSPHRASE = "qwer1234"
 
 API_KEY = os.getenv('HRK_KEY')
 API_SECRET = os.getenv('HRK_SECRET')
+NOTIFY_KEY = os.getenv('NOTIFY_KEY')
--- notify.py
+++ notify.py
@@ -8,17 +8,17 @@ headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'B
 
 
 def send(side,btc_amount,price,usdt_amount,time):
-    if(side == "buy"):
+    if(side == "BUY"):
         line1 = f"The Bot just bought : {btc_amount} BTC"
         line2 = f"At Price : {price} USDT"
         line3 = f"For {usdt_amount} USDT"
-    else:
+    elif (side == "SELL"):
         line1 = f"The Bot just sold : {btc_amount} BTC"
         line2 = f"At Price : {price} USDT"
         line3 = f"For : {usdt_amount} USDT"
 
     line4 = f"Time : {time}"
-    message = f"\n{line1}🚀🚀🚀\n{line2}\n{line3}\n{line4}"
+    message = f"\n{line1} 🚀🚀🚀\n{line2}\n{line3}\n{line4}"
     r = requests.post(url, headers=headers , data = {'message':message})
     print (r.text)
     return r.text
